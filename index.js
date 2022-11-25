981f2339b280523bf9f46ab98e9f363901cf0c90
maqiie <candiemp2580@gmai.com>
2022-11-25 13:23:36 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+//multiplying
+const num1 = 31;
+const num2 = 2;
+const multiply =num1*num2
+console.log(num1*num2);
 
-// Write your code here
+let integer = 1;
+const random = 1;
+
+//
+const num3 =8
+const num4 =2
+let mod = num3/num2
+
+//
+let max = Math.max(20)
